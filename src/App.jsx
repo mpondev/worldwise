ea881982b8a4d341ff7fe01529a28c7fa8b946c3
Mario Ponce <mario@mpondev.com>
2023-06-26 20:44:27 +0200

    Build pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
+import Login from './pages/Login';
+import AppLayout from './pages/AppLayout';
 import PageNotFound from './pages/PageNotFound';
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,10 +1,7 @@
-import PageNav from '../components/PageNav';
-
 function PageNotFound() {
   return (
     <div>
-      <PageNav />
-      <h1>Page not found :(</h1>
+      <h1>Page not found 😢</h1>
     </div>
   );
 }
