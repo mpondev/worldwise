05e3804dc40ad43ead8dd97e99df48f7c0727d83
Mario Ponce <mario@mpondev.com>
2023-06-29 16:25:46 +0200

    Fetch city data in the form

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -11,21 +11,20 @@ import {
 
 import { useCities } from '../context/CitiesContext';
 import { useGeolocation } from '../hooks/useGeolocation';
+import { useUrlPosition } from '../hooks/useUrlPosition';
 import Button from './Button';
 import styles from './Map.module.css';
 
 function Map() {
   const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
-  const [searchParams] = useSearchParams();
   const {
     isLoading: isLoadingPosition,
     position: geolocationPosition,
     getPosition,
   } = useGeolocation();
 
-  const mapLat = searchParams.get('lat');
-  const mapLng = searchParams.get('lng');
+  const [mapLat, mapLng] = useUrlPosition();
 
   useEffect(() => {
     if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
